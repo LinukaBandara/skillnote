f30114d3506d4b53f9893cdb4440e80e92a4d8cb
LinukaBandara <linukaipad@gmail.com>
2026-09-21 00:55:53 +0530

    fix: close hero JSX after removing readiness card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -138,7 +138,6 @@ export default async function Home() {
                   </div>
                 </div>
               </div>
-              </div>
             </div>
           </div>
         </div>
